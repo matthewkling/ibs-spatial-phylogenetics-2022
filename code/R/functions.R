242ce6dd7bc1bf5b96f759c16ceafee6d6ed4a90
Matthew Kling <mattkling@gmail.com>
2022-10-14 09:58:24 -0400

    updated scripts post-workshop

--- code/R/functions.R
+++ code/R/functions.R
@@ -43,7 +43,7 @@ clean_dataset <- function(x){
 }
 
 # a set of functions to construct a community matrix
-# with a row for every branch of a phylogeny
+# with a column for every branch of a phylogeny, not just every tip
 parentProb <- function(x) 1 - prod(1 - x)
 build_clade_range <- function(e, phylo, comm){
       node <- phylo$edge[e,2]
@@ -62,7 +62,7 @@ build_clade_ranges <- function(tree, comm){
              build_clade_range, phylo=tree, comm=comm)
 }
 
-# helpher function to plot a phylogeny
+# helpher function to plot a phylogeny with edge colors
 phyplot <- function(tree,
                    connect = NULL, hl = "orange", bg = "black",
                    value = NULL, col = c("black", "blue", "red", "orange"),
@@ -83,23 +83,16 @@ phyplot <- function(tree,
       plot(tree, edge.color = clr, ...)
 }
 
-# helper function to plot a map
+# helper function to ggplot a map
 carto <- function(d, v){
-      # md <- map_data("state", "california")
-      # md <- map_data("state")
       p <- ggplot() +
             geom_tile(data = d,
                       aes_string("x", "y", fill = v)) +
-            # geom_path(data = md,
-            #           aes(long, lat, group = group),
-            #           color = "white", alpha = .5) +
             xlim(range(d$x) + diff(range(d$x))/20 * c(-1, 1)) +
             ylim(range(d$y) + diff(range(d$y))/20 * c(-1, 1)) +
             coord_fixed(ratio = 1.2) +
             theme_void() +
             theme(legend.position = "top")
-      # theme(legend.position = c(.8, .95),
-      #       legend.justification = c(1, 1))
       if(inherits(d[[v]], "numeric")) p <- p + scale_fill_viridis_c()
       p
 }
